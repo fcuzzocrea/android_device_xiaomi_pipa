d884fa2aec18f1d86252d8e722c306dae688c276
Davide Garberi <dade.garberi@gmail.com>
2023-11-27 20:55:15 +0100

    pipa: rootdir: Use US for WiFi country code
     * Considering that the only two versions of this device
       are `GLOBAL` and `CN`, and that the former already sets
       `US` as countrycode regardless of anything, set it for
       the `CN` version as well, since this version of the device
       is mostly used and imported all over the world.
    
    Change-Id: Iaf6b0c1ae202425c0c83d2eb51ce5cec651d6876

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.device.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
