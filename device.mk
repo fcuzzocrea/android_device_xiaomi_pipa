638eb4421a4c82c64435185a9ae0aaba14af6a41
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-12 18:31:29 +0200

    pipa: Convert overlays to RRO
    Change-Id: Id0226559b81e115a6340591e7a107d25d7ad6848

--- device.mk
+++ device.mk
@@ -30,9 +30,13 @@ PRODUCT_PACKAGES += \
     libpiex_shim
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    ApertureOverlayDevice \
+    FrameworkResOverlayDevice \
+    LineageSettingsOverlayDevice \
+    LineageSystemUIOverlayDevice \
+    SettingsProviderOverlayDevice \
+    SystemUIOverlayDevice
 
 # Permissions
 PRODUCT_COPY_FILES += \
